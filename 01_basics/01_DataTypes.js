281e5888d7fea055b73191d0867b5259896693dd
Malhotra4354 <149765209+Malhotra4354@users.noreply.github.com>
2023-12-19 09:31:31 +0000

    datatypes

---
+++ 01_basics/01_DataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3 +3) //we are using node.js, not browser
+
+console.log(3+3);
+
+console.log("manish");
+
+let name = "manish"
+let age = 19
+let isLoogedin = false
+let state
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isLoogedin)
+console.log(typeof symbol)
+console.log(typeof undifined)//undifined
+console.log(typeof null)//object
