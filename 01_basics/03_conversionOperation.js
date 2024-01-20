324133c0292046079deb01cabaf59478b8c97037
Malhotra4354 <149765209+Malhotra4354@users.noreply.github.com>
2024-01-20 14:45:24 +0530

    Add files via upload

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,10 +12,53 @@ let ValueInNumber = Number(score)
 // "33abc" => Nan (Not a number)
 // true => 1; False => 0
 
-let isLoogedIn = "MANISH"
+let isLoogedIn = 0;
 
 
 let BooleanIsLoogedIn = Boolean(isLoogedIn)
-console.log(typeof BooleanIsLoogedIn);
-console.log(BooleanIsLoogedIn);
+// console.log(typeof BooleanIsLoogedIn);
+// console.log(BooleanIsLoogedIn);
 
+// 1 => TRUE
+// 0 => FALSE
+// "" => FALSE
+// "HITESH" => TRUE
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+// ****************Operations********************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+
+console.log(gameCounter);
